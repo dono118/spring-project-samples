483addef47b708727bdbd125d0f3543a92c1dc46
dono118 <dono118@126.com>
2020-01-05 17:00:53 +0800

    init commit

---
+++ spring-boot-web-jsp/src/main/java/com/icode/springboot/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.icode.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author Unicorn
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("msg", "你好!");
+        return "success";
+    }
+}
